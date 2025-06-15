b151868945904741f2c626513782c36a756a4bbf
suryaprabhat <maddalasuryaprabhat@gmail.com>
2025-06-15 21:20:52 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://chatapp-eight-pied.vercel.app/"],
     credentials: true,
   })
 );
